e80eb1201ac3897049fb50a3a32b5295e4f87293
BishalAshiq <bishaltheboss20@gmail.com>
2022-07-05 17:09:45 +0600

    added moreItems

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navigation from '../../../Shared/Navigation/Navigation';
+import PortalHeader from '../PortalHeader/PortalHeader';
 
 const Home = () => {
     return (
         <div>
             <Navigation></Navigation>
+            <PortalHeader></PortalHeader>
         </div>
     );
 };
--- src/Shared/Navigation/Navigation.js
+++ src/Shared/Navigation/Navigation.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React from "react";
+import setting from "../../Images/Nav/Settings.png";
 
 const Navigation = () => {
-    return (
-        <div>
-            <h1>Hello From Nav</h1>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        backgroundColor: "#2B0F81",
+        color: "#ffff",
+        alignItems: "center",
+        padding: "10px 0px",
+        overflow: "hidden",
+        flexWrap: "wrap",
+      }}
+    >
+      <h2 style={{ fontSize: 20, fontWeight: 400 }}>Back</h2>
+      <h2
+        style={{
+          fontSize: 20,
+          fontWeight: 400,
+          alignItems: "center",
+          textAlign: "center",
+          marginRight: 500,
+          marginLeft: 500,
+        }}
+      >
+        My Stat
+      </h2>
+      <img style={{ width: 170, height: 110 }} src={setting} alt="" />
+    </div>
+  );
 };
 
 export default Navigation;
